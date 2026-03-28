290376c70d96eb5ac6ef26b5bb56fa7d94911e95
khaleelulvarisep <khaleelulvaris@gmail.com>
2026-03-28 17:18:03 +0530

    order status

--- core/urls.py
+++ core/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
-    path('api/orders/',include('order.urls')),
+    path('api/orders/',include('order.urls')), 
     path('api/delivery/',include('delivery.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
--- order/serializers.py
+++ order/serializers.py
@@ -1,25 +1,3 @@
-from rest_framework import serializers
-from .models import Order
-
-
-# class OrderSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = Order
-#         fields = [
-#             'id',
-#             'customer',
-#             'grocery_items',
-#             'note',
-#             'latitude',
-#             'longitude',
-#             'status',
-#             'created_at'
-#         ]
-
-#         read_only_fields = ['customer', 'status', 'created_at']
-
-
 
 from rest_framework import serializers
 from .models import ChatMessage
@@ -38,19 +16,6 @@ class OrderSerializer(serializers.ModelSerializer):
             'created_at'
         ]
 
-
-# class ChatMessageSerializer(serializers.ModelSerializer):
-#     username = serializers.CharField(source="sender.email")
-
-#     class Meta:
-#         model = ChatMessage
-#         fields = ["id", "message", "username", "timestamp"]
-
-
-
-
-
-from rest_framework import serializers
 
 class ChatMessageSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(source="sender.id")
